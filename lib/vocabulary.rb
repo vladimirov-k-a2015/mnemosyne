5578b118e9899b20e1935fc489066ffb14d8004f
vladimirov-k-a2015 <sheyan.anton@gmail.com>
2016-04-13 23:11:25 +0300

    get arndom words array - ready - corrected 2

--- lib/vocabulary.rb
+++ lib/vocabulary.rb
@@ -3,7 +3,7 @@ class Vocabulary
 	def pick_random_line
 	  chosen_line = nil
 	  File.foreach("#{Rails.root}/lib/word_rus.txt").each_with_index do |line, number|
-	    chosen_line = line if rand < 1.0/(number+1)
+	    chosen_line = line.strip if rand < 1.0/(number+1)
 	  end
 	  return chosen_line
 	  puts chosen_line
