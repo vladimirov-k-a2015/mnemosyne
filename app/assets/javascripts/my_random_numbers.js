6a7872ac472d845a6caace591b3c59fd176f80a3
vladimirov-k-a2015 <sheyan.anton@gmail.com>
2016-04-18 20:25:22 +0300

    words list by button click

--- app/assets/javascripts/my_random_numbers.js
+++ app/assets/javascripts/my_random_numbers.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
 	var peremennaya;
+	var i = 0; 
+
 
 	$('.create-random-array').click(function() {
 		var n = $('#random-words-number').val();
@@ -16,7 +18,15 @@ $(document).ready(function() {
 	});
 
 
-	$('.anton').click(function() {
-		$(this).text(peremennaya); 
+	$('#show-next-word').click(function() {
+		if ( i < peremennaya.length ) 
+			{
+				$("#words-list").append("<li>" + peremennaya[i] + "</li>");
+				i += 1;
+			}
+			else
+		        { alert( "Показаны все слова" ); }
 	});
 });
+
+
