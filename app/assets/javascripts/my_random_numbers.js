41d91f2d784e62c8aa8fbd8cc3104fc22e3b5cda
vladimirov-k-a2015 <sheyan.anton@gmail.com>
2016-05-10 15:49:33 +0300

    add resrt

--- app/assets/javascripts/my_random_numbers.js
+++ app/assets/javascripts/my_random_numbers.js
@@ -28,32 +28,62 @@ $(document).ready(function() {
         return false;
     });
 
+    $("#reset-all").click(function(){
+        $("#random-numbers-list").text("");
+        $("#words-list").text("");
+        i = 0;
+        $("#one-word").text("");
+        j = 0;
+        $("#random-num").text("");
+        randomWordsArray = null;
+    });
+
+    $("#list-hide-show").click(function(){
+        $("#random-numbers-list").slideToggle("slow");
+    });
+
     $("#show-next-word").click(function() {
         if ( i < randomWordsArray.length ) {
             $("#words-list").append("<li>" + randomWordsArray[i] + "</li>");
             i += 1;
         } else {
-            alert("Показаны все слова");
-            i = 0;
+            // alert("Показаны все слова");
             $("#words-list").text("");
+            $("#words-list").append("<li>" + randomWordsArray[0] + "</li>");
+            i = 1;
         }
+    });
+
+    $("#reset-list").click(function(){
+        $("#words-list").text("");
+        i = 0;
     });
 
     $("#show-one-word").click(function() {
         if ( j < randomWordsArray.length ) {
-            $("#one-word").text(randomWordsArray[j]);
+            $("#one-word").text(j+1 + " -> " + randomWordsArray[j]);
             j += 1;
         } else {
-            alert("Показаны все слова.");
-            j = 0;
-            $("#one-word").text("");
+            // alert("Показаны все слова.");
+            $("#one-word").text("1 -> " + randomWordsArray[0]);
+            j = 1;
+            // $("#one-word").text("");
         }
+    });
+
+    $("#reset-one-word").click(function(){
+        $("#one-word").text("");
+        j = 0;
     });
 
     $("#show-random-number").click(function() {
         randomNum = getRandomInt(randomWordsArray);
         $("#random-num").text(randomNum + 1);
         console.log(randomNum);
+    });
+
+    $("#reset-random-number").click(function(){
+        $("#random-num").text("");
     });
 
 });
